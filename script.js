afc348fc5bbaa8c402ebdd5e0300138b6869cb53
marcelladrian <marcell.dctr2@gmail.com>
2021-04-07 10:33:10 +0700

    Var vs Const

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log(1)
+// ES 5
+var myName = "Ariq";
+myName = "Rifki";
+console.log(myName);
+
+
+const myCar = "Mazda EX-7"
+// myCar = "Honda Freed"
+// myCar = "Honda ABC"
+console.log(myCar);
