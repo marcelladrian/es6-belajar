c70778fba152f907113e4735a6e59f0854b01386
marcelladrian <marcell.dctr2@gmail.com>
2021-04-07 11:48:15 +0700

    belajar foreach

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('hello');
+//ambil element
+const skills = document.getElementById("skills");
+
+//buat variabel
+const mySkills =  ["ES6", "UX Design", "Git"]
+
+// error
+// mySkills = ["Copywriting", "Translator"];
+
+console.log(mySkills);
+
+//function
+function showSkill(mySkill){ //normal function
+    console.log(mySkill);
+}
+
+const showSkill2 = (mySkill) => { //arrow function
+    console.log(mySkill);
+};
+
+
+//forEach
+
+mySkills.forEach(function (mySkill) { //iterator function
+    console.log(mySkill);
+});
+
+let parent = '<ul>'
+
+mySkills.forEach((mySkill) => { //iterator function
+    // console.log(mySkill);
+    parent += `<li>${mySkill}</li>`
+});
+
+parent += '</ul>'
+
+mySkills.forEach(showSkill);
+skills.innerHTML = parent;
