7beb7317e682752e48a9109e989f15bd8f6156b3
marcelladrian <marcell.dctr2@gmail.com>
2021-04-07 11:20:28 +0700

    belajar object literal

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello');
+//object literals
+const cart = document.getElementById("cart")
+
+//ES5
+
+function addProduct(name, category) {
+    return {name, category};
+}
+
+var getProduct = addProduct("iPhone 12", "Smartphone");
+var getProduct2 = addProduct("iPhone 12 Pro", "Smartphone");
+
+console.log(getProduct);
+console.log(getProduct2);
+
+cart.innerHTML = `Product 1: ${getProduct.name}, category ${getProduct.category}`;
