938d9047d7eb5b2b727a9f80ae276463f28b8845
marcelladrian <marcell.dctr2@gmail.com>
2021-04-07 11:07:08 +0700

    belajar string concatenation

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello');
+// console.log('hello');
+
+//ambil element user
+const user = document.getElementById('user');
+
+//init variabel
+const user_logged_in = "Marcell";
+
+// ES5
+// user.innerHTML = "Selamat datang, " + user_logged_in;
+
+//ES6
+user.innerHTML = `Anda berhasil login, ${user_logged_in}`;
