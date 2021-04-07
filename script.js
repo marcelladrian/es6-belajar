52d35eb752ed221cad846aa1743c9dac051b1441
marcelladrian <marcell.dctr2@gmail.com>
2021-04-07 13:20:49 +0700

    learning map and filter

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('hello');
+//ambil element
+const skills = document.getElementById("skills")
+const primarySkill = document.getElementById("primarySkill")
+
+const mySkills = ["ES6", "Git", "Web Development", "Web Design"];
+
+// const printSkills = mySkills.map((skill) => {
+//     return skill;
+// });
+
+//map
+//undefined
+const printSkillsForEach = mySkills.map((skill) => {
+    return skill;
+});
+//benar
+const printSkillsMap = mySkills.map((skill) => {
+    return skill;
+});
+
+
+skills.innerHTML = printSkillsMap;
+
+//filter
+const printPrimarySkill = mySkills.filter((skill) => {
+    return skill != "ES6"
+});
+
+primarySkill.innerHTML = printPrimarySkill
