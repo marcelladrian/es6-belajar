bbc1b232bf74a4079088c690b52fb3e9763022e6
marcelladrian <marcell.dctr2@gmail.com>
2021-04-07 10:55:06 +0700

    Modif 2-let

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('hello');
+// ES 5
+// var myName = "Ariq";
+// myName = "Richard";
+// console.log(myName);
+
+var myCarsVar = ["BMW", "Tesla", "Toyota"];
+
+for(var i = 0; i < myCarsVar.length; i++){
+    console.log("My Cars var = " + myCarsVar[i]);
+}
+
+console.log(i);
+
+// ES 6
+// let myNameLet = "Ariq";
+// myNameLet = "Calvin";
+// console.log(myNameLet);
+
+const myCarsLet = ["BMW", "Tesla", "Toyota"];
+
+for (let x = 0; x < myCarsLet.length; x++){
+    console.log("My Cars let = " + myCarsLet[x]);
+}
+
+console.log(x);
+
+
+// function abc() {
+//     var myNameFunc = "hello";
+//     console.log(myNameFunc);
+// }
+
+// abc();
+// console.log(myNameFunc);
